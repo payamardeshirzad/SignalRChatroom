efc2ea53dbe68c03ba95c2a28bbbf11a77f2b99b
Payam Ardeshirzad <payam@ardeshirzad.com>
2020-04-22 09:53:46 +1200

    initial commit - move to github repository

---
+++ SignalR_ChatRoom/Hubs/chathub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalR_ChatRoom.Hubs
+{
+    public class chathub : Hub
+    {
+        static int numofonlines = 0;
+        public void Send(string name, string message)
+        {
+            Clients.All.addnewmessage(name, message,++numofonlines);
+        }
+    }
+}
